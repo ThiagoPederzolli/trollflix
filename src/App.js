3719ed1f6d3c929b60b28224a23036bdcda3ba0f
ThiagoPederzolli <thiago_pederzolli@yahoo.com.br>
2020-07-27 21:56:23 -0300

    Primeira parte do projeto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Menu from './components/Menu'
+import dadosIniciais from './data/dados_iniciais.json';
+import BannerMain from './components/BannerMain';
+import Carousel from './components/Carousel';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ background: "#141414" }}>
+      <Menu />
+
+      <BannerMain
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={"Olá! Você acessou a TrollFlix. Aqui você encontrará dicas de séries, músicas, canais de programação e jogos. Será uma forma de compartilhar com o mundo conteúdos que acho incríveis e espero que agradem alguns de vocês também. A TrollFlix surgiu como projeto da Imersão React da Alura. Não deixa de acompanhar a Alura nas redes sociais para ficar por dentro de todo esse conteúdo sensacional e confere a plataforma, são mais de mil cursos, quem sabe tu encontra aquele que faz o coração bater mais rápido?!"}
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[0]}
+      />
+
+      <Carousel
+        category={dadosIniciais.categorias[1]}
+      />
+
+      <Carousel
+        category={dadosIniciais.categorias[2]}
+      />             
+
+      <Carousel
+        category={dadosIniciais.categorias[4]}
+      />      
+
+        
+
+      <Footer />
     </div>
   );
 }
